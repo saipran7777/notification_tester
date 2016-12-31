e22460e4b2d9bf86a1392bf945710ddedf90c88f
KeerthanaS <keerukeerthana8@gmail.com>
2016-12-31 07:16:04 +0530

    Send Notification Request

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $http) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -33,6 +33,47 @@ angular.module('starter.controllers', [])
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
 
+    var link = 'https://fcm.googleapis.com/fcm/send';
+    xhr = new XMLHttpRequest();
+    xhr.open("POST", link, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.setRequestHeader("Authorization", 
+                        "key=AAAAYcotsXc:APA91bEMKXX0Yd-kyohNf9yiAeIvrp7fP7m-BN2kTIdkVe4H6DJRQ-MCradcbkVEhLCgEMN4K1TgbOS8AMv5ZmrXzMNhmofQg3MxxDffrlSznsdjm5zi10RkTi8301GHIqpz3UGzZXIyv6Tpb3mQ44b-c7Ya94Rlpg");
+    xhr.onreadystatechange = function () { 
+      console.log("Response");
+        var json = JSON.parse(xhr.responseText);
+        console.log(json);
+        console.log(xhr.status);
+
+}
+  // $.ajax({
+  //               url: "https://iid.googleapis.com/iid/v1/" + currentToken +"/rel/topics/notifs",
+  //               type: "POST",
+  //               beforeSend: function(request) {
+  //                 request.setRequestHeader("Authorization", 
+  //                   "AAAAYcotsXc:APA91bEMKXX0Yd-kyohNf9yiAeIvrp7fP7m-BN2kTIdkVe4H6DJRQ-MCradcbkVEhLCgEMN4K1TgbOS8AMv5ZmrXzMNhmofQg3MxxDffrlSznsdjm5zi10RkTi8301GHIqpz3UGzZXIyv6Tpb3mQ44b-c7Ya94Rlpg");
+  //               },
+  //               dataType: "json",
+  //               success: function (result) {
+  //                       console.log(result);
+  //               },
+  //               error: function (xhr, ajaxOptions, thrownError) {
+  //               console.log(xhr.status);
+  //               console.log(thrownError);
+  //               }
+  //           });
+
+    var data = JSON.stringify({
+      "to": "/topics/all",
+       "notification": {
+         "title": "Welcome Message",
+         "body": "This is a Firebase Cloud Messaging Topic Message!"
+       }
+    });
+    console.log(data);
+
+    xhr.send(data);
+    //$http.post(link, )
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
     $timeout(function() {
